c2ab48693593b5706ef414684fc4bb42b4c68054
Lucas Zordan <lucaszordan0204@gmail.com>
2021-10-06 13:23:27 -0300

    implementing 'list users service' test

--- src/useCases/listUsers/listUsersService.js
+++ src/useCases/listUsers/listUsersService.js
@@ -5,20 +5,22 @@ class ListUsersService {
     static async execute() {
         let users = await User.getAllUsers()
 
-        if (users) {
-            users = users.map(user => {
-                const { id, name, email, admin, createdAt, updatedAt } = user
-
-                return {
-                    id,
-                    name,
-                    email,
-                    admin,
-                    createdAt,
-                    updatedAt
-                }
-            })
+        if (!users) {
+            throw new Error('Error! No users found!')
         }
+
+        users = users.map(user => {
+            const { id, name, email, admin, createdAt, updatedAt } = user
+
+            return {
+                id,
+                name,
+                email,
+                admin,
+                createdAt,
+                updatedAt
+            }
+        })
 
         return users
     }
---
+++ src/useCases/listUsers/listUsersService.spec.js
@@ -0,0 +1,49 @@
+const { describe, test, expect, jest: mock } = require('@jest/globals')
+const listUsersService = require('./listUsersService')
+const User = require('../../model/User')
+
+describe('List users service test suite', () => {
+    test('should list all users', async () => {
+        const mockUsers = [
+            {
+              "id": 1,
+              "name": "Test one",
+              "email": "test@test1.com",
+              "admin": false,
+              "createdAt": "2021-09-27T16:07:51.000Z",
+              "updatedAt": "2021-09-27T16:07:51.000Z"
+            },
+            {
+              "id": 2,
+              "name": "Test two",
+              "email": "test@test2.com",
+              "admin": false,
+              "createdAt": "2021-09-27T16:07:51.000Z",
+              "updatedAt": "2021-09-27T16:07:51.000Z"
+            }
+        ]
+
+        mock.spyOn(User, User.getAllUsers.name)
+            .mockResolvedValue(mockUsers)
+
+        const users = await listUsersService.execute()
+
+        expect(users).toStrictEqual(mockUsers)
+    })
+
+    test('should throw an exception', async () => {
+        mock.spyOn(User, User.getAllUsers.name)
+            .mockResolvedValue(false)
+
+        let response;
+
+        try {
+            response = await listUsersService.execute()
+        } catch(err) {
+            expect(err).toBeInstanceOf(Error)
+            expect(err.message).toBe('Error! No users found!')
+        }
+
+        expect(response).toBe(undefined)
+    })
+})
